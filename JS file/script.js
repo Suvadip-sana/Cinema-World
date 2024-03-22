85a655150a838dc255e9286d46fa221aa909d969
Suvadip Sana <suvadipsana602@gmail.com>
2024-03-22 13:22:59 +0530

    Add files via upload

---
+++ JS file/script.js
@@ -0,0 +1,36 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Fetch the error message element
+    var errorMessage = document.querySelector("center font[color='red'] p");
+    
+    // Fetch the 'butt' div
+    var buttDiv = document.querySelector("h1");
+    
+    // Insert the error message above the 'butt' div
+    if (errorMessage && buttDiv) {
+        buttDiv.parentNode.insertBefore(errorMessage, buttDiv);
+        errorMessage.style.color = "red";
+    }
+});
+
+var serverSideValidationPassed = true;
+
+function validateForm() {
+    if (!serverSideValidationPassed) {
+        alert("Server-side validation failed. Please check your inputs.");
+        return false;
+    }
+
+    var mobileNumber = document.getElementsByName("mob")[0].value;
+    var errorMessage = "";
+
+    // Regular expression to check if the mobile number contains string values
+    var mobileRegex = /[a-zA-Z]/;
+
+    if (mobileRegex.test(mobileNumber)) {
+        errorMessage = "Mobile number should not contain string values.";
+        alert(errorMessage);
+        return false;
+    } else {
+        return true;
+    }
+}
